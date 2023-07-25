21c275b6cc5ae50b9fb79ac430d74701e151b566
“RichardGSchmidt” <richard.g.schmidt.jr@gmail.com>
2023-07-25 14:32:28 -0500

    Update: adds P1 Q3 question comments.

--- practice-test-1/question3.py
+++ practice-test-1/question3.py
@@ -1,3 +1,12 @@
+#The Fibonacci sequence begins with 0 and then 1 follows. 
+#All subsequent values are the sum of the previous two, ex: 0, 1, 1, 2, 3, 5, 8, 13. 
+#Complete the fibonacci() function, which has an index n as parameter and returns the nth value in the sequence. 
+#Any negative index values should return -1.
+
+#Ex: If the input is: 7
+#the output is: 'fibonacci(7) is 13'
+
+
 def fibonacci(n):
     first = 0
     second = 1
