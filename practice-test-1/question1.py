8f58c5830081166e516c2b6bf9c58d86684387a6
“RichardGSchmidt” <richard.g.schmidt.jr@gmail.com>
2023-07-25 14:36:47 -0500

    Fixed: Adds question text comments for Q1 P1.

--- practice-test-1/question1.py
+++ practice-test-1/question1.py
@@ -1,2 +1,11 @@
+#33.1 LAB: Formatted output: No parking sign
+
+#Write a program that prints a formatted "No parking" sign as shown below. 
+#Note the first line has two leading spaces. 
+#For ALL labs, end with newline (unless otherwise stated).
+
+#  NO PARKING
+#2:00 - 6:00 a.m.
+
 print('  NO PARKING')
 print('2:00 - 6:00 a.m.')
